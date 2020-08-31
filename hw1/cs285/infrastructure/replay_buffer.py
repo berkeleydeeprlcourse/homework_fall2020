8b43acba04e145bb9d9563ec8798666700ee1efa
Vitchyr Pong <vitchyr@gmail.com>
2020-08-31 12:38:40 -0700

    fix TODO statement

--- hw1/cs285/infrastructure/replay_buffer.py
+++ hw1/cs285/infrastructure/replay_buffer.py
@@ -72,12 +72,11 @@ class ReplayBuffer(object):
                 == self.terminals.shape[0]
         )
 
-        # TODO return batch_size number of random entries from each of the 5
-        # component arrays above
-        # HINT 1: use np.random.permutation to sample random indices
-        # HINT 2: return corresponding data points from each array (i.e., not
-        #         different indices from each array)
-        # HINT 3: look at the sample_recent_data function below
+        ## TODO return batch_size number of random entries from each of the 5 component arrays above
+        ## HINT 1: use np.random.permutation to sample random indices
+        ## HINT 2: return corresponding data points from each array (i.e., not different indices from each array)
+        ## HINT 3: look at the sample_recent_data function below
+
         return TODO, TODO, TODO, TODO, TODO
 
     def sample_recent_data(self, batch_size=1):
