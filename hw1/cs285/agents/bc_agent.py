6e37404a5d1eac2e6c8d7874cf486037600d5ac8
Vitchyr Pong <vitchyr@gmail.com>
2020-08-31 12:37:19 -0700

    specify import

--- hw1/cs285/agents/bc_agent.py
+++ hw1/cs285/agents/bc_agent.py
@@ -1,5 +1,5 @@
 from cs285.infrastructure.replay_buffer import ReplayBuffer
-from cs285.policies.MLP_policy import *
+from cs285.policies.MLP_policy import MLPPolicySL
 from .base_agent import BaseAgent
 
 
